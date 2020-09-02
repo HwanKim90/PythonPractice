b33f7495e730dcac2cdea11cf5fe4d2918cd5a76
HwanKim90 <70571766+HwanKim90@users.noreply.github.com>
2020-09-02 18:41:41 +0900

    The first Python Project add to the Github

---
+++ pythonTutorial/tutorial1.py
@@ -0,0 +1,14 @@
+spam_amount = 0
+print(spam_amount)
+
+# spam_amount = spam_amount + 4
+spam_amount += 4
+
+if spam_amount > 0:
+    print("But I don't want ANY spam!")
+
+viking_song = "spam " * spam_amount
+print(viking_song)
+
+print(type(spam_amount))
+
