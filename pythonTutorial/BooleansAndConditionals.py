4305e56fe1d0595f0ee406ad792fbf332fc82a78
HwanKim90 <70571766+HwanKim90@users.noreply.github.com>
2020-09-04 17:09:35 +0900

    Conditionals

--- pythonTutorial/BooleansAndConditionals.py
+++ pythonTutorial/BooleansAndConditionals.py
@@ -21,3 +21,32 @@ print("Is 100 odd?", is_odd(100))
 print("Is -1 odd?", is_odd(-1))
 
 print(True or True and False)
+
+def inspect(x):
+    if x == 0:
+        print(x, "is zero")
+    elif x > 0:
+        print(x, "is positive")
+    elif x < 0:
+        print(x, "is negative")
+    else:
+        print(x, "is unlike anything I've ever seen...")
+
+inspect(0)
+inspect(-15)
+
+def f(x):
+    if x > 0:
+        print("Only printed when x is positive; x =", x)
+        print("Also only printed when x is positive; x=", x)
+    print("Always printed, regardless of x's value; x=", x)
+
+f(1)
+f(0)
+
+print(bool(1)) # all numbers are treated as true, except 0
+print(bool(2))
+print(bool(0))
+print(bool("asf")) # all strings are treated as true, except the empty string ""
+print(bool(""))
+# Generally empty sequence (strings, lists, tuples)
