5959bb2353619fe74c992705134ebb88b170cc2e
HwanKim90 <70571766+HwanKim90@users.noreply.github.com>
2020-09-05 22:58:20 +0900

    add lists to pythontutorial

---
+++ pythonTutorial/Lists.py
@@ -0,0 +1,29 @@
+primes = [2, 3, 5, 7]
+
+planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'jupiter', 'Saturn', 'Uranus', 'Neptune']
+
+hands = [
+    ['J', 'Q', 'K'],
+    ['2', '2', '2'],
+    ['6', 'A', 'K'],
+]
+
+hands = [['J', 'Q', 'K'], ['2', '2', '2'], ['6', 'A', 'K']]
+
+my_favorite_things = [26, 'League of legend', help]
+
+print(planets[0])
+print(planets[1])
+print(planets[-1])
+print(planets[-2])
+print("-------------------------")
+print(planets[0:3])
+print(planets[:3])
+print(planets[3:])
+print(planets[1:-1])
+print(planets[-3:])
+
+
+
+
+
