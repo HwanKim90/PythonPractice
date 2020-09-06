bfb0a1ed671eaea150cc4a78c68501dcf82d2a03
HwanKim90 <70571766+HwanKim90@users.noreply.github.com>
2020-09-06 17:41:57 +0900

    lists2

--- pythonTutorial/Lists.py
+++ pythonTutorial/Lists.py
@@ -23,6 +23,44 @@ print(planets[3:])
 print(planets[1:-1])
 print(planets[-3:])
 
+planets[3] = 'Malacandra'
+print(planets)
+print("----------------------------------------")
+planets[:3] = ['Mur', 'Vee', 'Ur']
+print(planets)
+planets[:4] = ['Mercury', 'Venus', 'Earth', 'Mars']
+print(planets)
+
+length = len(planets)
+print(length)
+
+sort = sorted(planets)
+print(sort)
+
+primes = [2, 3, 4, 5]
+print("sum!!! : ", sum(primes))
+
+print(max(primes))
+
+planets.append('Pluto')
+print(planets)
+
+planets.pop()
+print(planets)
+
+print(planets.index('Earth'))
+print("Earth" in planets)
+
+t = (1, 2, 3)
+print(t)
+
+a = 1
+b = 0
+a, b = b, a
+print(a, b)
+
+
+
 
 
 
